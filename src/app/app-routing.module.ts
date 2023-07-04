cff3f0f6aaba0399af7ce5067d914dc2887d4b0f
SriSomanaath <govindalapudisrinath@gmail.com>
2023-07-04 23:33:36 +0530

    initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { Testing1Component } from './testing1/testing1.component';
+
+const routes: Routes = [
+  {
+    path:'',
+    component:Testing1Component
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
